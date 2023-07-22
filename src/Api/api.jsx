f7006d3ede3b2d541bbc47a010a2eceadd90458a
Parthib Chowdhury <parthibchowdhury31@gmail.com>
2023-07-22 18:40:54 +0530

    Environtment Variable Updated

--- src/Api/api.jsx
+++ src/Api/api.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
+import dotenv from "dotenv";
 
-const URL = "http://localhost:8000";
+dotenv.config();
+const URL = process.env.BackendURL;
 
 export const getNotes = async(id) => {
     id = id || ''
